d3a5e8795f721d3cd79301f6e9a066b4d8816e44
1Batmain <batduval@gmail.com>
2026-02-25 21:38:17 +0100

    Working convolution Layer model avec linference et la backpropagation in

--- modele/src/main.rs
+++ modele/src/main.rs
@@ -11,27 +11,27 @@ async fn main() {
     let mut model = Model::new(None).await;
     model.add_layer(LayerSpec::Convolution(ConvolutionLayerSpec {
         nb_kernel: 10,
-        dim_kernel: Dim3 {x:3, y:3, z:1},
+        dim_kernel: Dim3 {x:1, y:1, z:1},
         stripe: 1,
         mode: PaddingMode::Valid,
         dim_input: Some(Dim3 {x:200, y:20, z:1}),
     }));
     model.add_layer(LayerSpec::Convolution(ConvolutionLayerSpec {
         nb_kernel: 1,
-        dim_kernel: Dim3 {x:3, y:3, z:10},
+        dim_kernel: Dim3 {x:1, y:1, z:10},
         stripe: 1,
         mode: PaddingMode::Valid,
         dim_input: None,
     }));
     model.add_layer(LayerSpec::Convolution(ConvolutionLayerSpec {
         nb_kernel: 1,
-        dim_kernel: Dim3 {x:3, y:3, z:1},
+        dim_kernel: Dim3 {x:1, y:1, z:1},
         stripe: 1,
         mode: PaddingMode::Valid,
         dim_input: None,
     }));
     model.build_model();
-    dbg!(model);
-    //let res = model.run().await;
-    //println! ("Result({}) : {:?}",res.len(), res);
+    let res = model.infer(vec![1.; 200 * 20]).await;
+    println! ("Result({}) : {:?}", res.len(), res);
+    model.save("name");
 }
